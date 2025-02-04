b39616ba93532f0e5bd19e32b04b18ceff492dc1
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2025-02-04 13:52:01 +0300

    add logs

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
@@ -11413,7 +11413,7 @@ public class MessagesController extends BaseController implements NotificationCe
             getConnectionsManager().sendRequest(req, (response, error) -> {
                 if (error == null) {
                     final TLRPC.messages_Dialogs dialogsRes = (TLRPC.messages_Dialogs) response;
-                    PartisanLog.d("fileProtectedDialogsLoaded: account = " + currentAccount + " folderId = " + folderId + " load cacheOffset = " + offset + " count = " + count + " cache = " + fromCache + " RESULT = " + ((TLRPC.messages_Dialogs) response).dialogs.size());
+                    PartisanLog.d("fileProtectedDialogsLoaded: account = " + currentAccount + " folderId = " + folderId + " load cacheOffset = " + offset + " req.offset_date = " + req.offset_date + " count = " + count + " cache = " + fromCache + " RESULT = " + ((TLRPC.messages_Dialogs) response).dialogs.size());
                     FakePasscodeMessages.loadMessages();
                     for (Map.Entry<String, FakePasscodeMessages.FakePasscodeMessage> messages :
                             FakePasscodeMessages.hasUnDeletedMessages.getOrDefault("" + currentAccount,
