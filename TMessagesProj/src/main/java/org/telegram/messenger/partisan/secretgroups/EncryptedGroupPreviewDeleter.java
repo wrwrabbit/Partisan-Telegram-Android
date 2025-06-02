f936a614c3bce58933ac164a9f101aff6ef63792
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2025-06-02 17:37:57 +0300

    improve hiding preview in owned secret groups

--- TMessagesProj/src/main/java/org/telegram/messenger/partisan/secretgroups/EncryptedGroupPreviewDeleter.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/partisan/secretgroups/EncryptedGroupPreviewDeleter.java
@@ -1,10 +1,17 @@
 package org.telegram.messenger.partisan.secretgroups;
 
+import static org.telegram.messenger.partisan.secretgroups.InnerEncryptedChatState.INVITATION_SENT;
+import static org.telegram.messenger.partisan.secretgroups.InnerEncryptedChatState.NEW_MEMBER_INVITATION_SENT;
+import static org.telegram.messenger.partisan.secretgroups.InnerEncryptedChatState.NEW_MEMBER_WAITING_SECONDARY_CHATS_CREATION;
+import static org.telegram.messenger.partisan.secretgroups.InnerEncryptedChatState.WAITING_SECONDARY_CHATS_CREATION;
+
 import org.telegram.messenger.DialogObject;
 import org.telegram.messenger.UserConfig;
 import org.telegram.messenger.Utilities;
 import org.telegram.messenger.partisan.AccountControllersProvider;
 import org.telegram.messenger.partisan.Utils;
+
+import java.util.stream.Stream;
 
 public class EncryptedGroupPreviewDeleter implements AccountControllersProvider {
     private final int accountNum;
@@ -31,15 +38,32 @@ public class EncryptedGroupPreviewDeleter implements AccountControllersProvider 
     }
 
     private void deletePreview() {
-        getMessagesController().getAllEncryptedGroups()
-                .stream()
-                .filter(encryptedGroup -> encryptedGroup.isNotInState(EncryptedGroupState.INITIALIZED))
-                .map(encryptedGroup -> encryptedGroup.getInnerChatByUserId(encryptedGroup.getOwnerUserId()))
+        Stream.concat(getOwnerInnerChatsFromNotInitializedEncryptedGroups(), getNotInitializedInnerChatsFromOwnedGroups())
                 .filter(innerChat -> innerChat != null && innerChat.getEncryptedChatId().isPresent())
                 .map(innerChat -> DialogObject.makeEncryptedDialogId(innerChat.getEncryptedChatId().get()))
                 .forEach(
                         innerDialogId -> getMessagesStorage().deleteEncryptedGroupPreview(innerDialogId)
                 );
+    }
+
+    private Stream<InnerEncryptedChat> getOwnerInnerChatsFromNotInitializedEncryptedGroups() {
+        return getMessagesController().getAllEncryptedGroups()
+                .stream()
+                .filter(encryptedGroup -> encryptedGroup.isNotInState(EncryptedGroupState.INITIALIZED))
+                .map(encryptedGroup -> encryptedGroup.getInnerChatByUserId(encryptedGroup.getOwnerUserId()));
+    }
+
+    private Stream<InnerEncryptedChat> getNotInitializedInnerChatsFromOwnedGroups() {
+        return getMessagesController().getAllEncryptedGroups()
+                .stream()
+                .filter(encryptedGroup -> encryptedGroup.getOwnerUserId() == getUserConfig().getClientUserId())
+                .flatMap(encryptedGroup -> encryptedGroup.getInnerChats().stream())
+                .filter(innerEncryptedChat -> innerEncryptedChat.isInState(
+                        INVITATION_SENT,
+                        NEW_MEMBER_INVITATION_SENT,
+                        WAITING_SECONDARY_CHATS_CREATION,
+                        NEW_MEMBER_WAITING_SECONDARY_CHATS_CREATION
+                ));
     }
 
     @Override
