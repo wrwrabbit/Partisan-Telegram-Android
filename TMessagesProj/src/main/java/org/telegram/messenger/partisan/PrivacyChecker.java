ed84284e64f857b114552b71bbc07f71317d1117
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2025-10-16 02:24:33 +0300

    fix build

--- TMessagesProj/src/main/java/org/telegram/messenger/partisan/PrivacyChecker.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/partisan/PrivacyChecker.java
@@ -53,9 +53,9 @@ public class PrivacyChecker implements NotificationCenter.NotificationCenterDele
     private static void setupPrivacySettings(int account, int rulesType, Runnable onError, Runnable onSuccess) {
         TL_account.setPrivacy req = new TL_account.setPrivacy();
         if (rulesType == PRIVACY_RULES_TYPE_PHONE) {
-            req.key = TLRPC.InputPrivacyKey.inputPrivacyKeyPhoneNumber;
+            req.key = new TLRPC.TL_inputPrivacyKeyPhoneNumber();
             TL_account.setPrivacy req2 = new TL_account.setPrivacy();
-            req2.key = TLRPC.InputPrivacyKey.inputPrivacyKeyAddedByPhone;
+            req2.key = new TLRPC.TL_inputPrivacyKeyAddedByPhone();
             req2.rules.add(new TLRPC.TL_inputPrivacyValueAllowContacts());
             ConnectionsManager.getInstance(account).sendRequest(req2, (response, error) -> AndroidUtilities.runOnUIThread(() -> {
                 if (error == null) {
@@ -64,19 +64,19 @@ public class PrivacyChecker implements NotificationCenter.NotificationCenterDele
                 }
             }), ConnectionsManager.RequestFlagFailOnServerErrors);
         } else if (rulesType == PRIVACY_RULES_TYPE_FORWARDS) {
-            req.key = TLRPC.InputPrivacyKey.inputPrivacyKeyForwards;
+            req.key = new TLRPC.TL_inputPrivacyKeyForwards();
         } else if (rulesType == PRIVACY_RULES_TYPE_PHOTO) {
-            req.key = TLRPC.InputPrivacyKey.inputPrivacyKeyProfilePhoto;
+            req.key = new TLRPC.TL_inputPrivacyKeyProfilePhoto();
         } else if (rulesType == PRIVACY_RULES_TYPE_BIO) {
-            req.key = TLRPC.InputPrivacyKey.inputPrivacyKeyAbout;
+            req.key = new TLRPC.TL_inputPrivacyKeyAbout();
         } else if (rulesType == PRIVACY_RULES_TYPE_P2P) {
-            req.key = TLRPC.InputPrivacyKey.inputPrivacyKeyPhoneP2P;
+            req.key = new TLRPC.TL_inputPrivacyKeyPhoneP2P();
         } else if (rulesType == PRIVACY_RULES_TYPE_CALLS) {
-            req.key = TLRPC.InputPrivacyKey.inputPrivacyKeyPhoneCall;
+            req.key = new TLRPC.TL_inputPrivacyKeyPhoneCall();
         } else if (rulesType == PRIVACY_RULES_TYPE_INVITE) {
-            req.key = TLRPC.InputPrivacyKey.inputPrivacyKeyChatInvite;
+            req.key = new TLRPC.TL_inputPrivacyKeyChatInvite();
         } else {
-            req.key = TLRPC.InputPrivacyKey.inputPrivacyKeyStatusTimestamp;
+            req.key = new TLRPC.TL_inputPrivacyKeyStatusTimestamp();
         }
 
         req.rules.add(new TLRPC.TL_inputPrivacyValueDisallowAll());
